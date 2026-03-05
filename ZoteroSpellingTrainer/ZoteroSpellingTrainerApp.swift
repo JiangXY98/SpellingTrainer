8b68c109039f31d4980f581c49b5054dde244cdd
Xiaoyu Jiang <gooycity@gmail.com>
2026-03-05 15:49:44 +0800

    Initial Commit

---
+++ ZoteroSpellingTrainer/ZoteroSpellingTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoteroSpellingTrainerApp.swift
+//  ZoteroSpellingTrainer
+//
+//  Created by Xiaoyu Jiang on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct ZoteroSpellingTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
